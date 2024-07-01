cae79fb2f05333bef52081aeba7910a1b014f800
harshyadav114 <108623715+harshyadav114@users.noreply.github.com>
2024-07-01 14:54:16 +0530

    Add files via upload

---
+++ StudyBud/base/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser,BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields = {"is_staff": False, "is_superuser": False, **extra_fields}
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = User(email=email, **extra_fields)
+
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+
+        extra_fields = {**extra_fields, "is_staff": True, "is_superuser": True}
+
+        user = self.create_user(email=email, password=password, **extra_fields)
+
+        return user
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
+
+    avatar = models.ImageField(null=True, default="avatar.svg")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects=UserManager()
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.body[0:50]
